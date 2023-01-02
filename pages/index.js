6ea086393100d0ce16a502c19157f15ebe0e102f
mwaraem <mwaraem@gmail.com>
2023-01-02 14:29:02 +0300

    final commit

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,31 @@
+import { useState } from 'react';
 import CoinList from '../components/CoinList'
 import SearchBar from '../components/SearchBar'
+import Layout from '../components/Layout'
 
 
 export default function Home({ filteredCoins }) {
-  return (
-    <div>
+  const [search, setSearch] = useState('')
 
-      <SearchBar type='text' placeholder='Search' />
-      <CoinList filteredCoins={filteredCoins} />
-    </div>
+  const allCoins = filteredCoins.filter(coin =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  )
+
+  const handleChange = e => {
+    e.preventDefault()
+
+    setSearch(e.target.value.toLowerCase())
+  }
+
+
+
+  return (
+    <Layout>
+      <div className='coin_app'>
+        <SearchBar type='text' placeholder='Search' onChange={handleChange} />
+        <CoinList filteredCoins={allCoins} />
+      </div>
+    </Layout>
   );
 }
 
