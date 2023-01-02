9c43cf77a0e91ef3da0b3448b7e98f1e6f9ec87d
mwaraem <mwaraem@gmail.com>
2023-01-02 13:06:34 +0300

    initial commit

---
+++ components/Coins/index.js
@@ -0,0 +1,34 @@
+
+const Coins = ({ name, price, symbol, marketcap, volume, image, priceChange, id }) => {
+    return (
+        <div className="coin_container">
+            <div className="coin_row">
+                <div className="coin">
+                    <img src={image} alt={name} className="coin_img" />
+                    <h1 className="coin_h1">{name}</h1>
+                    <p className="coin_symbol">{symbol}</p>
+                </div>
+                <div className="coin_data">
+                    <p className="coin_price">${price}</p>
+
+                    {priceChange < 0 ? (
+                        <p className="coin_percent red">
+                            {priceChange.toFixed(2)}%
+                        </p>
+                    ) : (
+                        <p className="coin_percent green">
+                            {priceChange.toFixed(2)}%
+                        </p>
+                    )}
+
+                    <p className="coin_marketcap">
+                        Mkt cap: ${marketcap.toLocaleString()}
+                    </p>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Coins
